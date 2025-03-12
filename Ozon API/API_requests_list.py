70d3abc9331858d3d8cb22d18952f8edb622ee10
Samurai-netizen <ssamurmurai@gmail.com>
2025-03-12 17:38:46 +0300

    Разделил на папки запросы на Ozon API, чтобы можно было лаконично делать запросы по требованию

--- Ozon API/API_requests_list.py
+++ Ozon API/API_requests_list.py
@@ -2,13 +2,13 @@ import asyncio
 import niquests
 import json
 
+url = "https://api-seller.ozon.ru"
+headers = {'Host': 'api-seller.ozon.ru', 'Client-Id': '2435732', 'Api-Key': '2ddf55f0-05e5-484e-9655-d424f72a0e82', "Content-Type": "application/json"}
 
 async def v1_warehouse_list() -> None:
     async with niquests.AsyncSession() as s:
-        url = "https://api-seller.ozon.ru"
         method = "/v1/warehouse/list"
         print(url + method)
-        headers = {'Host': 'api-seller.ozon.ru', 'Client-Id': '2435732', 'Api-Key': '2ddf55f0-05e5-484e-9655-d424f72a0e82', "Content-Type": "application/json"}
         payload = {"offer_id": "DOM6B", "product_id": 1855823959}
         payload = json.dumps(payload)
 
@@ -18,4 +18,28 @@ async def v1_warehouse_list() -> None:
             print(item['name'])
         print("--------------------------------------------------------------")
 
+
+async def v5_product_info_prices() -> None:
+    async with niquests.AsyncSession() as s:
+        method = "/v5/product/info/prices"
+        print(url + method)
+        payload = {
+          "cursor": "string",
+          "filter": {
+            "offer_id": [
+              "DOM6B"
+            ],
+            "product_id": [
+              "1855823959"
+            ],
+            "visibility": "ALL"
+          },
+          "limit": 1
+        }
+        payload = json.dumps(payload)
+
+        r = await s.post(url + method, headers=headers, params=payload)
+        print(r.json())
+
+        print("--------------------------------------------------------------")
 
--- Ozon API/Ozon_api_init.py
+++ Ozon API/Ozon_api_init.py
@@ -1,9 +1,10 @@
 import asyncio
-from API_requests_list import v1_warehouse_list
+from API_requests_list import v1_warehouse_list, v5_product_info_prices
 
 
 async def main() -> None:
     await v1_warehouse_list()
+    await v5_product_info_prices()
 
 
 if __name__ == "__main__":
